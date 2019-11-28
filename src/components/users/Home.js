fbf7f518fecfb153b09b58e673becd9d31c8f659
Lizzie <lchan217@gmail.com>
2019-11-28 10:20:05 -0700

    add form and change component to class

--- src/components/users/Home.js
+++ src/components/users/Home.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Form } from "semantic-ui-react";
 
-const Home = () => {
-  return (
-    <div>
-      <Container>test</Container>
-      <i class="reply icon"></i>
-    </div>
-  );
-};
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Container>
+          <h1>Please Log In</h1>
+          <Form>
+            <Form.Field>
+              <label>Email</label>
+              <input placeholder="Email" />
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input placeholder="Password" />
+            </Form.Field>
+          </Form>
+          <br />
+          NEED TO IMPLEMENT: New User? Sign Up Here
+        </Container>
+      </div>
+    );
+  }
+}
 
 export default Home;
