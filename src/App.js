55027a5800c3f5f4a99d9fc310b9df1602ae74b2
Lizzie <lchan217@gmail.com>
2019-11-27 18:51:41 -0700

    delete containers from app js, add userform file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import './App.css';
-import AuthorContainer from './containers/AuthorContainer'
-import BookContainer from './containers/BookContainer'
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div>
       Welcome!
-    <br/>
+      <br />
       Add login screen here once user model is set up
-      <AuthorContainer />
-      <BookContainer />
     </div>
   );
 }
