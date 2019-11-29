ea2139e024f7084d4c245dbf48380aab4f085554
Lizzie <lchan217@gmail.com>
2019-11-29 15:18:59 -0700

    add fetch_books reducer

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,5 +1,8 @@
 export default function booksReducer(state = [], action) {
   switch (action.type) {
+    case "FETCH_BOOKS":
+      debugger;
+      return action.payload;
     default:
       return state;
   }
