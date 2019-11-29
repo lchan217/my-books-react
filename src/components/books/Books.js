8eb371316c6c9eeda137681d63c4a3f83ca880ec
Lizzie <lchan217@gmail.com>
2019-11-29 15:19:15 -0700

    add mapstate to props and dispatch

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchBooks } from "../../actions/bookActions.js";
 
 class Books extends Component {
+  componentDidMount() {
+    this.props.fetchBooks();
+  }
+
+  showBooks() {}
+
   render() {
     return <div>Books Component</div>;
   }
 }
 
-export default Books;
+const mapStatetoProps = state => {
+  return {
+    books: state.books
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchBooks: id => dispatch({ type: "FETCH_BOOKS", payload: id })
+  };
+};
+
+export default connect(mapStatetoProps, mapDispatchToProps)(Books);
