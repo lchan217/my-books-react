96fe4f7d8e92af524c222d35d8e4ccff7f59c021
Lizzie <lchan217@gmail.com>
2019-11-29 15:17:45 -0700

    add fetchBooks

---
+++ src/actions/bookActions.js
@@ -0,0 +1,9 @@
+const fetchBooks = id => {
+  debugger;
+  return dispatch => {
+    return fetch("http://localhost:3001/api/books")
+      .then(response => response.json())
+      .then(books => console.log(books))
+      .then(books => dispatch({ type: "FETCH_BOOKS", payload: books }));
+  };
+};
