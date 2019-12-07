7e3315848de130542627628f984759ed5bcc0170
Lizzie <lchan217@gmail.com>
2019-12-07 15:00:13 -0700

    add add_book case

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,5 +1,9 @@
 export default function booksReducer(state = [], action) {
   switch (action.type) {
+    case "ADD_BOOK":
+      debugger;
+      const news = [...state, action.book];
+      return news;
     default:
       return state;
   }
