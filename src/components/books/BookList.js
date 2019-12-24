6448cb8e631950c4402497129bb13fba09da6b18
Lizzie <lchan217@gmail.com>
2019-12-23 21:49:40 -0700

    add edit and delete links

--- src/components/books/BookList.js
+++ src/components/books/BookList.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BookList = props => {
   return (
     <div>
       booklist <br />
       {props.books.map((book, idx) => (
-        <li key={idx}>{book.title}</li>
+        <li key={idx}>
+          {book.title} <Link to='/edit'>Edit</Link>{" "}
+          <Link to='/delete'>delete </Link>
+        </li>
       ))}{" "}
     </div>
   );
