79a8ad120926804e6af5f351ebc84e2c37c5a2de
Lizzie <lchan217@gmail.com>
2019-12-23 21:49:56 -0700

    add edit route & component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NewUserForm from "./components/users/NewUserForm";
 import BookContainer from "./components/books/BookContainer";
 import NewBookForm from "./components/books/NewBookForm";
+import EditBookForm from "./components/books/EditBookForm";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/books' component={BookContainer} />
         <Route exact path='/newBook' component={NewBookForm} />
+        <Route exact path='/edit' component={EditBookForm} />
       </Router>
     </div>
   );
---
+++ src/components/books/EditBookForm.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import { Button, Form } from "semantic-ui-react";
+import { newBook } from "../../actions/bookActions";
+import { connect } from "react-redux";
+import $ from "jquery";
+
+class EditBookForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      author: "",
+      rating: "",
+      review: "",
+      pages: "",
+      genre: "",
+      author_id: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    let token = "Bearer " + localStorage.getItem("jwt");
+    let body = {
+      title: this.state.title,
+      author: this.state.author,
+      rating: this.state.rating,
+      review: this.state.review,
+      pages: this.state.pages,
+      genre: this.state.genre,
+      img: this.state.img
+    };
+    console.log(`token: ${token}`);
+    $.ajax({
+      url: "http://localhost:3001/api/books",
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      data: JSON.stringify(body),
+      dataType: "json",
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("Authorization", token);
+      },
+      context: this,
+      success: function(results) {
+        console.log(results);
+        window.location.href = "/books";
+        this.setState({
+          title: "",
+          author: "",
+          rating: "",
+          review: "",
+          pages: "",
+          genre: "",
+          img: ""
+        });
+      }
+    });
+  };
+
+  render() {
+    const { title, author, rating, review, pages, genre, img } = this.state;
+    const { handleChange, handleSubmit } = this;
+
+    return (
+      <div>
+        new book form
+        <Form onSubmit={handleSubmit}>
+          <label>Title</label>
+          <input
+            type='text'
+            name='title'
+            onChange={handleChange}
+            value={title}
+            placeholder='Title
+                '
+          ></input>
+          <label>Author</label>
+          <input
+            type='text'
+            name='author'
+            onChange={handleChange}
+            value={author}
+            placeholder='Author
+                '
+          ></input>
+          <label>Rating</label>
+          <input
+            type='text'
+            name='rating'
+            onChange={handleChange}
+            value={rating}
+            placeholder='Rating
+                '
+          ></input>
+          <label>Review</label>
+          <input
+            type='text'
+            name='review'
+            onChange={handleChange}
+            value={review}
+            placeholder='Review
+                '
+          ></input>
+          <label>Pages</label>
+          <input
+            type='text'
+            name='pages'
+            onChange={handleChange}
+            value={pages}
+            placeholder='Pages
+                '
+          ></input>
+          <label>Genre</label>
+          <input
+            type='text'
+            name='genre'
+            onChange={handleChange}
+            value={genre}
+            placeholder='Genre
+                '
+          ></input>
+          <label>Book Cover</label>
+          <input
+            type='text'
+            name='img'
+            onChange={handleChange}
+            value={img}
+            placeholder='Book Cover
+                '
+          ></input>
+          <Button> Submit </Button>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { newBook })(EditBookForm);
