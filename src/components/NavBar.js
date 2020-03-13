1d90a9e59754a8b9a9cd989fb2bf0d94c17023d8
Lizzie <lchan217@gmail.com>
2020-03-13 10:01:53 -0700

    change navbar to class component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
-const NavBar = () => {
-  return (
-    <div className='navBar'>
-      <Menu>
-        <Menu.Item className='menu-item' as={Link} to='/feed'>
-          Home
-        </Menu.Item>
-        <Menu.Item className='menu-item' as={Link} to='/books'>
-          My Books
-        </Menu.Item>
-        <Menu.Item className='menu-item' as={Link} to='/not-sure-yet'>
-          Browse
-        </Menu.Item>
-        <Menu.Item className='menu-item' as={Link} to='/not-sure-yet'>
-          Community
-        </Menu.Item>
-      </Menu>
-    </div>
-  );
-};
+class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: ""
+    };
+  }
+  render() {
+    return (
+      <div className='navBar'>
+        <Menu>
+          <Menu.Item className='menu-item' as={Link} to='/feed'>
+            Home
+          </Menu.Item>
+          <Menu.Item className='menu-item' as={Link} to='/books'>
+            My Books
+          </Menu.Item>
+          <Menu.Item className='menu-item' as={Link} to='/not-sure-yet'>
+            Browse
+          </Menu.Item>
+          <Menu.Item className='menu-item' as={Link} to='/not-sure-yet'>
+            Community
+          </Menu.Item>
+        </Menu>
+      </div>
+    );
+  }
+}
 
 export default NavBar;
