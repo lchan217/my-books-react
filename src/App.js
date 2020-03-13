89ca785a084ae8cc071f5ab5cc57da326638e134
Lizzie <lchan217@gmail.com>
2020-03-13 09:33:54 -0700

    add skeleton to navbar and links

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NewUserForm from "./components/users/NewUserForm";
 import BookContainer from "./components/books/BookContainer";
 import NewBookForm from "./components/books/NewBookForm";
-import EditBookForm from "./components/books/EditBookForm";
+import AllBooks from "./components/books/AllBooks";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <Router>
+        <NavBar />
         <Route exact path='/newUser' component={NewUserForm} />
         <Route exact path='/' component={Home} />
         <Route exact path='/books' component={BookContainer} />
         <Route exact path='/newBook' component={NewBookForm} />
+        <Route exact path='/all-books' component={AllBooks} />
       </Router>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+
+const NavBar = () => {
+  return (
+    <div className='navBar'>
+      <Menu>
+        <Menu.Item className='menu-item' as={Link} to='/all-books'>
+          Home
+        </Menu.Item>
+        <Menu.Item className='menu-item' as={Link} to='/books'>
+          My Books
+        </Menu.Item>
+        <Menu.Item className='menu-item' as={Link} to='/not-sure-yet'>
+          Browse
+        </Menu.Item>
+        <Menu.Item className='menu-item' as={Link} to='/not-sure-yet'>
+          Community
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default NavBar;
